2eac915cecd39774af27ee5769a77709d4e2c677
tomwilson <tomas.f.wilson@gmail.com>
2021-11-05 13:38:54 -0300

    feat: ensure cors is enabled

---
+++ tests/main/middlewares/cors.test.ts
@@ -0,0 +1,20 @@
+import app from '@/main/config/app'
+import request from 'supertest'
+
+describe('Cors middleware', () => {
+  test('should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    const res = await request(app).get('/test_cors')
+    console.log('res.header', res.header)
+    expect(res.header['access-control-allow-origin']).toBe('*')
+    expect(res.header['access-control-allow-methods']).toBe(
+      'GET, POST, PUT, DELETE'
+    )
+    expect(res.header['access-control-allow-headers']).toBe(
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+  })
+})
