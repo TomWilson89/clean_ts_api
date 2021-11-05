05a547c1df589afcbccaaa1129ff7f5da41c7836
tomwilson <tomas.f.wilson@gmail.com>
2021-11-05 11:53:12 -0300

    test: ensure AccountMongoRepository returns valid id

---
+++ tests/infra/db/mongodb/account-repository.spec.ts
@@ -0,0 +1,36 @@
+import { AddAccountRepository } from '@/data/protocols'
+import { AccountMongoRepository, MongoHelper } from '@/infra'
+
+interface SutTpes {
+  sut: AddAccountRepository
+}
+
+const makeSut = (): SutTpes => {
+  const sut = new AccountMongoRepository()
+
+  return {
+    sut
+  }
+}
+
+describe('Account Mongo Repositorty', () => {
+  beforeAll(async () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await MongoHelper.connect(process.env.MONGO_URL!)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('should return a valid id on success', async () => {
+    const { sut } = makeSut()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+  })
+})
