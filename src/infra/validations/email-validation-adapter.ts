df8e6fde4b6859143a385f822c0a0958105d5539
tomwilson <tomas.f.wilson@gmail.com>
2021-11-04 23:12:32 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/infra/validations/email-validation-adapter.ts
+++ src/infra/validations/email-validation-adapter.ts
@@ -1,7 +1,8 @@
 import { EmailValidator } from '@/validations/protocots'
+import validator from 'validator'
 
 export class EmailValidationAdapter implements EmailValidator {
   async isValid(email: string): Promise<boolean> {
-    return false
+    return validator.isEmail(email)
   }
 }
