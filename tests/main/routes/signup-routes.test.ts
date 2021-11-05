7f3738d7569c1b122c7a74577f8f6d71b3310da5
tomwilson <tomas.f.wilson@gmail.com>
2021-11-05 17:19:42 -0300

    refactor: connect and disconnet from mongo in integration test

--- tests/main/routes/signup-routes.test.ts
+++ tests/main/routes/signup-routes.test.ts
@@ -1,7 +1,22 @@
+import { MongoHelper } from '@/infra'
 import app from '@/main/config/app'
 import request from 'supertest'
 
 describe('SignUp routes', () => {
+  beforeAll(async () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await MongoHelper.connect(process.env.MONGO_URL!)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountColletion = MongoHelper.getCollection('accounts')
+    await accountColletion.deleteMany({})
+  })
+
   test('should return an account on success', async () => {
     const res = await request(app).post('/api/signup').send({
       name: 'John Doe',
