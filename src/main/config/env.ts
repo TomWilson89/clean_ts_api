399930a3dded496d561d7b8a567d979d469d2a21
tomwilson <tomas.f.wilson@gmail.com>
2021-11-09 15:30:03 -0300

    chore: add and fix env variable names

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoRul: process.env.MONRO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT ?? 4000,
   jwtSecret: process.env.JWT_SECRET ?? '9poa78wefh342#Q$R#"ASD'
 }
