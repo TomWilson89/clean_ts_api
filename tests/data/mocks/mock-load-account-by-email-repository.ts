f1fe47d8efbe2ee3c82bad1232dd5aa721360b52
tomwilson <tomas.f.wilson@gmail.com>
2021-11-11 14:02:52 -0300

    refactor: change LoadAccountByEmailRepositoryStub so can easily modify return value

--- tests/data/mocks/mock-load-account-by-email-repository.ts
+++ tests/data/mocks/mock-load-account-by-email-repository.ts
@@ -4,13 +4,16 @@ import { AccountModel } from '../../../src/domain/models'
 export class LoadAccountByEmailRepositoryStub
   implements LoadAccountByEmailRepository
 {
+  email: string
+  account: AccountModel = {
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@mail.com',
+    password: 'hashed_password'
+  }
+
   async loadByEmail(email: string): Promise<AccountModel> {
-    const account: AccountModel = {
-      id: 'valid_id',
-      name: 'valid_name',
-      email: 'valid_email@mail.com',
-      password: 'hashed_password'
-    }
-    return account
+    this.email = email
+    return this.account
   }
 }
