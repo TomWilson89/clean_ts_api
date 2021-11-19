5e0c1da673679cdbe2ea2edbba72662d29f16c97
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-19 08:27:49 -0300

    fix: ensure AddControllerValidation calls wit httpRequest body

--- src/presentation/controller/add-survey-controller.ts
+++ src/presentation/controller/add-survey-controller.ts
@@ -4,7 +4,7 @@ export class AddSurveyController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.validation.validate(httpRequest)
+    await this.validation.validate(httpRequest.body)
     return null
   }
 }
