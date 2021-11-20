7747aa81dc8db894a628d110393cdf4781872e62
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-19 22:49:58 -0300

    feat: ensuire AuthMiddleware returns 500 if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,21 +1,25 @@
 import { LoadAccountByToken } from '../../domain/usecases'
 import { AccessDeniedError } from '../errors'
-import { forbidden, successResponse } from '../helpers'
+import { forbidden, serverError, successResponse } from '../helpers'
 import { HttpRequest, HttpResponse, Middleware } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
   constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken)
-      if (account) {
-        return successResponse({
-          accountId: account.id
-        })
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken)
+        if (account) {
+          return successResponse({
+            accountId: account.id
+          })
+        }
       }
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
     }
-    return forbidden(new AccessDeniedError())
   }
 }
