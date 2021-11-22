c4a15bfff080725868753542a2fcc71fc09aa33b
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-22 11:07:28 -0300

    feat: ensuyre SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey-mongo-repository.ts
@@ -1,10 +1,21 @@
 import { MongoHelper } from '.'
-import { AddSurveyRepository } from '../../../data/protocols/'
+import {
+  AddSurveyRepository,
+  LoadSurveysRepository
+} from '../../../data/protocols/'
+import { SurveyModel } from '../../../domain/models'
 import { AddSurveyModel } from '../../../domain/usecases'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+type SurveyRepositoryTypes = AddSurveyRepository & LoadSurveysRepository
+export class SurveyMongoRepository implements SurveyRepositoryTypes {
   async add(data: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(data)
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = (await surveyCollection.find().toArray()) as SurveyModel[]
+    return surveys
   }
 }
