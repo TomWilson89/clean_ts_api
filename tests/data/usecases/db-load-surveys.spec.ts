f050d06406e05186670b07f9ef21c81b520c46f7
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-22 10:41:10 -0300

    test: ensure DbLoadSurveys returns a list of surveys on success

--- tests/data/usecases/db-load-surveys.spec.ts
+++ tests/data/usecases/db-load-surveys.spec.ts
@@ -1,8 +1,25 @@
 import { LoadSurveysRepository } from '../../../src/data/protocols'
 import { DbLoadSurveys } from '../../../src/data/usecases'
+import { SurveyModel } from '../../../src/domain/models'
 import { LoadSurveys } from '../../../src/domain/usecases'
 import { LoadSurveysRepositoryStub } from '../mocks/'
 
+const makeFakeSurvey = (): SurveyModel[] => {
+  const fakeSurveys = [
+    {
+      id: 'valid_id',
+      question: 'valid_question',
+      answers: [
+        {
+          image: 'valid_image',
+          answer: 'valid_answer'
+        }
+      ],
+      date: new Date()
+    }
+  ]
+  return fakeSurveys
+}
 interface SutTypes {
   sut: LoadSurveys
   loadSurveysRepositoryStub: LoadSurveysRepository
@@ -24,5 +41,11 @@ describe('DbLoadSurveys', () => {
     const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
     await sut.load()
     expect(loadAllSpy).toHaveBeenCalled()
+  })
+
+  test('should return a list of surveys if LoadSurveysRepository success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.load()
+    expect(httpResponse).toEqual(makeFakeSurvey())
   })
 })
