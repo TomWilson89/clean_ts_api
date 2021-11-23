6a514476f599ea465556b0d4db208d51d6ecb78e
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-23 11:08:57 -0300

    test: ensure DbLoadSurveyById returns a survey on success

--- tests/data/mocks/mock-load-survey-by-id-repository.ts
+++ tests/data/mocks/mock-load-survey-by-id-repository.ts
@@ -3,6 +3,16 @@ import { SurveyModel } from '@domain/models'
 
 export class LoadSurveyByIdRepositoryStub implements LoadSurveyByIdRepository {
   async loadById(id: string): Promise<SurveyModel> {
-    return null
+    return {
+      id: 'valid_id',
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        }
+      ],
+      date: new Date()
+    }
   }
 }
--- tests/data/usecases/db-load-survey-by-id.spec.ts
+++ tests/data/usecases/db-load-survey-by-id.spec.ts
@@ -1,8 +1,23 @@
 import { LoadSurveyByIdRepository } from '@data/protocols'
 import { DbLoadSurveyById } from '@data/usecases'
+import { SurveyModel } from '@domain/models'
 import { LoadSurveysById } from '@domain/usecases'
 import MockDate from 'mockdate'
 import { LoadSurveyByIdRepositoryStub } from '../mocks'
+
+const makeFakeSurvey = (): SurveyModel => {
+  return {
+    id: 'valid_id',
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      }
+    ],
+    date: new Date()
+  }
+}
 
 type SutTypes = {
   sut: LoadSurveysById
@@ -31,5 +46,11 @@ describe('DbLoadSurveyById', () => {
     const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
     await sut.loadById('any_id')
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('should return a survey on success', async () => {
+    const { sut } = makeSut()
+    const survey = await sut.loadById('any_id')
+    expect(survey).toEqual(makeFakeSurvey())
   })
 })
