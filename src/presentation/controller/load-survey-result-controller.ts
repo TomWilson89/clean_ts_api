7d3f14a57fd5b6a8159d7480687e53d12b2d0ada
tomwilson89 <tfuenteswilson@gmail.com>
2021-11-29 12:15:32 -0300

    feat: ensure LoadSurveyResult controller calls LoadSurveyResult with correct values

--- src/presentation/controller/load-survey-result-controller.ts
+++ src/presentation/controller/load-survey-result-controller.ts
@@ -1,19 +1,23 @@
-import { LoadSurveysById } from '@domain/usecases'
+import { LoadSurveyResult, LoadSurveysById } from '@domain/usecases'
 import { InvalidParamError } from '@presentation/errors'
 import { forbidden, serverError } from '@presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols'
 
 export class LoadSurveyResultController implements Controller {
-  constructor(private readonly loadSurveyById: LoadSurveysById) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveysById,
+    private readonly loadSurveyResult: LoadSurveyResult
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(
-        httpRequest.params.surveyId
-      )
+      const { surveyId } = httpRequest.params
+      const survey = await this.loadSurveyById.loadById(surveyId)
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
+
+      await this.loadSurveyResult.load(surveyId)
       return null
     } catch (error) {
       return serverError(error)
